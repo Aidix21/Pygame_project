f2db14183fe5591a15919598fbed535a72e04c88
Aidix21 <aidar58147@mail.ru>
2023-02-26 23:15:17 +0300

    progress

--- first.py
+++ first.py
@@ -8,84 +8,140 @@ pygame.display.set_caption('Pygame_1')
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
 
-bg = pygame.image.load('images/bg.jpg')
+bg = pygame.image.load('images/bg.jpg').convert()
+
+ghost  = pygame.image.load('images/ghost.png').convert_alpha()
+ghost_list = []
+
 walk_left = [
-    pygame.image.load('images/player_left/player_left_1.png'),
-    pygame.image.load('images/player_left/player_left_2.png'),
-    pygame.image.load('images/player_left/player_left_3.png'),
-    pygame.image.load('images/player_left/player_left_4.png'),
-    pygame.image.load('images/player_left/player_left_5.png'),
-    pygame.image.load('images/player_left/player_left_6.png'),
-    pygame.image.load('images/player_left/player_left_7.png'),
-    pygame.image.load('images/player_left/player_left_8.png')]
+    pygame.image.load('images/player_left/player_left_1.png').convert_alpha(),
+    pygame.image.load('images/player_left/player_left_2.png').convert_alpha(),
+    pygame.image.load('images/player_left/player_left_3.png').convert_alpha(),
+    pygame.image.load('images/player_left/player_left_4.png').convert_alpha(),
+    pygame.image.load('images/player_left/player_left_5.png').convert_alpha(),
+    pygame.image.load('images/player_left/player_left_6.png').convert_alpha(),
+    pygame.image.load('images/player_left/player_left_7.png').convert_alpha(),
+    pygame.image.load('images/player_left/player_left_8.png').convert_alpha()]
 
 walk_right = [
-    pygame.image.load('images/player_right/player_right_1.png'),
-    pygame.image.load('images/player_right/player_right_2.png'),
-    pygame.image.load('images/player_right/player_right_3.png'),
-    pygame.image.load('images/player_right/player_right_4.png'),
-    pygame.image.load('images/player_right/player_right_5.png'),
-    pygame.image.load('images/player_right/player_right_6.png'),
-    pygame.image.load('images/player_right/player_right_7.png'),
-    pygame.image.load('images/player_right/player_right_8.png')]
+    pygame.image.load('images/player_right/player_right_1.png').convert_alpha(),
+    pygame.image.load('images/player_right/player_right_2.png').convert_alpha(),
+    pygame.image.load('images/player_right/player_right_3.png').convert_alpha(),
+    pygame.image.load('images/player_right/player_right_4.png').convert_alpha(),
+    pygame.image.load('images/player_right/player_right_5.png').convert_alpha(),
+    pygame.image.load('images/player_right/player_right_6.png').convert_alpha(),
+    pygame.image.load('images/player_right/player_right_7.png').convert_alpha(),
+    pygame.image.load('images/player_right/player_right_8.png').convert_alpha()]
 
 anim_player_count = 0
 bg_x = 0
 
-player_speed = 20
+player_speed = 20  # 5
 player_x = 100
 player_y = 225
 
 jump = False
-jump_count = 6
+jump_count = 7
 
+ghost_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(ghost_timer, 3000)
+gameplay = True
 
+label = pygame.font.Font('fonts/Lato-Regular.ttf', 40)
+lose_label = label.render('You lose!', False, (193, 196, 199))
+restart_label = label.render('restart', False, (115, 132, 148))
+restart_label_rect = restart_label.get_rect(topleft=(285, 220))
+bullet = pygame.image.load('images/wind.png').convert_alpha()
+bullets = []
 running = True
 while running:
 
     screen.blit(bg, (bg_x, 0))
     screen.blit(bg, (bg_x + 720, 0))
 
-    keys = pygame.key.get_pressed()
+    if gameplay:
+        player_rect = walk_left[0].get_rect(topleft=(player_x, player_y))
 
-    if keys[pygame.K_LEFT]:
-        screen.blit(walk_left[anim_player_count], (player_x, player_y))
-    else:
-        screen.blit(walk_right[anim_player_count], (player_x, player_y))
+        if ghost_list:
+            for (i, el) in enumerate(ghost_list):
+                screen.blit(ghost, el)
+                el.x -= 10
 
-    if keys[pygame.K_LEFT] and player_x > 20:
-        player_x -= player_speed
-    elif keys[pygame.K_RIGHT] and player_x < 680:
-        player_x += player_speed
+                if el.x < -40:
+                    ghost_list.pop(i)
 
-    if not jump:
-        if keys[pygame.K_SPACE]:
-            jump = True
-    else:
-        if jump_count >= -6:
-            if jump_count > 0:
-                player_y -= jump_count ** 2 / 2
-            else:
-                player_y += jump_count ** 2 / 2
-            jump_count -= 1
+                if player_rect.colliderect(el):
+                    gameplay = False
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            screen.blit(walk_left[anim_player_count], (player_x, player_y))
         else:
-            jump = False
-            jump_count = 6
+            screen.blit(walk_right[anim_player_count], (player_x, player_y))
 
-    if anim_player_count == 7:
-        anim_player_count = 0
+        if keys[pygame.K_LEFT] and player_x > 20:
+            player_x -= player_speed
+        elif keys[pygame.K_RIGHT] and player_x < 680:
+            player_x += player_speed
+
+        if not jump:
+            if keys[pygame.K_UP]:
+                jump = True
+        else:
+            if jump_count >= -7:
+                if jump_count > 0:
+                    player_y -= jump_count ** 2 / 2
+                else:
+                    player_y += jump_count ** 2 / 2
+                jump_count -= 1
+            else:
+                jump = False
+                jump_count = 7
+
+        if anim_player_count == 7:
+            anim_player_count = 0
+        else:
+            anim_player_count += 1
+
+        bg_x -= 2
+        if bg_x == -720:
+            bg_x = 0
+
+        if keys[pygame.K_SPACE]:
+            bullets.append(bullet.get_rect(topleft=(player_x + 30, player_y + 10)))
+
+        if bullets:
+            for (i, el) in enumerate(bullets):
+                screen.blit(bullet, (el.x, el.y))
+                el.x += 4
+
+                if el.x > 740:
+                    bullets.pop(i)
+
+                if ghost_list:
+                    for (index, ghost) in enumerate(ghost_list):
+                        if el.colliderect(ghost):
+
+
     else:
-        anim_player_count += 1
+        screen.fill((87, 88, 89))
+        screen.blit(lose_label, (270, 150))
+        screen.blit(restart_label, restart_label_rect)
 
-    bg_x -= 2
-    if bg_x == -720:
-        bg_x = 0
+        mouse = pygame. mouse.get_pos()
+        if restart_label_rect.collidepoint(mouse) and pygame.mouse.get_pressed()[0]:
+            gameplay = True
+            player_x = 100
+            ghost_list.clear()
+            bullets.clear()
 
     pygame.display.update()
+
+    clock.tick(10)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-
-    clock.tick(10)
+        if event.type == ghost_timer:
+            ghost_list.append(ghost.get_rect(topleft=(740, 240)))
